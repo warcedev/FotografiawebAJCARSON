ba30e9b6cb623a2bedba5c81dcd68a87e14b8548
warcedev <william.arcedev@gmail.com>
2026-01-31 21:46:59 -0300

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Importante para Vercel
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    port: 3000
   }
 })
